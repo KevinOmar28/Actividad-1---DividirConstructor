a18324e44fbe4c1c647da04f41c863feef6674ad
kevin <kevin@DESKTOP-CFB1PAG>
2022-09-23 01:20:36 -0600

    Agregar archivos de proyecto.

---
+++ POO - Actividad 1 - Dividir/Divicion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO___Actividad_1___Dividir
+{
+    internal class Divicion
+    {
+        public double num1;
+        public double num2;
+
+        public Divicion()
+        {
+        }
+
+        public Divicion(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+        public double divide()
+        {
+            return num1 / num2;
+        }
+    }
+}
